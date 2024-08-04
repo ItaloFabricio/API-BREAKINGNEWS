e780e002f99964fc638f8c8769e79807e6c108d0
ítalo Fabrício <italofps65@gmail.com>
2024-08-03 22:31:13 -0300

    refactor: added the software architecture

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require('express');
+const userRoute = require('./src/routes/user.route');
+
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/soma', userRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
