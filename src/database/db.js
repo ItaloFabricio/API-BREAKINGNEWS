590dbcfced5d006b64e226f3e6b73d2d7efe4a3b
Italo Fabrício <italofps65@gmail.com>
2024-08-10 21:23:01 -0300

    refactor: environment variables to protect sensitive data

--- src/database/db.js
+++ src/database/db.js
@@ -4,10 +4,10 @@ const connectDatabase = () => {
   console.log("Wait connecting to the database");
 
   mongoose
-    .connect(
-      "mongodb+srv://italofps65:wEKrYtyN2coP0GVY@cluster0.kbm99jy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB Atlas Connected"))
     .catch((error) => console.log(error));
 };
