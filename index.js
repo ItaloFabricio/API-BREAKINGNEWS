81ceaf8b5f8cbfa8401c3a76ae1680887db8d950
Italo Fabrício <italofps65@gmail.com>
2024-08-30 19:13:42 -0300

    refactoring the code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import connectDatabase from './src/database/db.js';
 import dotenv from 'dotenv';
 
@@ -12,8 +13,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+
 connectDatabase();
 app.use(express.json());
+app.use(cors());
 app.use('/user', userRoute);
 app.use('/auth', authRoute);
 app.use('/news', newsRoute);
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,10 +1,10 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
-const loginService = (email) =>
+export const loginService = (email) =>
   User.findOne({ email: email }).select("+password");
 
 //token para guardar a seção do usuário e o sistema saber qual o usuário está nessa seção sem expor os dados
-const generateToken = (id) => jwt.sign({id: id}, process.env.SECRET_JWT, {expiresIn: 86400});
+export const generateToken = (id) => jwt.sign({id: id}, process.env.SECRET_JWT, {expiresIn: 86400});
 
-export { loginService, generateToken };
+export default { loginService, generateToken };
