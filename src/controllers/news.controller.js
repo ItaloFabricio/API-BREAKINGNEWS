b75da562eec7b0162739fc285bc7477726d60f5d
Italo Fabrício <italofps65@gmail.com>
2024-08-30 17:17:14 -0300

    refact: return empty array

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -68,9 +68,9 @@ export const findAll = async (req, res) => {
         ? `${currentUrl}?limit=${limit}&offset=${previous}`
         : null;
 
-    if (news.length == 0) {
+    /* if (news.length === 0) {
       return res.status(400).send({ message: "There are not registered news" });
-    }
+    } */
 
     res.send({
       nextUrl,
